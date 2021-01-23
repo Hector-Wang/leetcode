629373da09b249484f3a9c6f695981438e6f1355
Wang Haofeng <whf_zju@163.com>
2021-01-23 20:09:44 +0800

    add uhash header files

---
+++ c_demo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "list_hash/hlist.h"
+#include "list_hash/hhash.h"
+
+int main(int argc, const char *argv[])
+{
+    printf("%s\n", "hello world!\n");
+}
+
