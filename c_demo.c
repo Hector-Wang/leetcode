34c31d1e9d2732ad3df2e6388a508965e35aac1a
Wang Haofeng <whf_zju@163.com>
2021-01-25 23:21:57 +0800

    finish daily subject 959. 由斜杠划分区域

--- c_demo.c
+++ c_demo.c
@@ -148,6 +148,89 @@ int* addToArrayForm(int* A, int ASize, int K, int* returnSize)
     return ret;
 }
 
+/*
+leetcode 959. 由斜杠划分区域
+https://leetcode-cn.com/problems/regions-cut-by-slashes/
+并查集
+*/
+
+void mergeRegions(char ** grid, int gridSize, int *parent)
+{
+    for (int i = 0; i < gridSize; ++i) {
+        for (int j = 0; j < gridSize; ++j) {
+            int baseId = (i * gridSize + j) * 4;
+            switch (grid[i][j]) {
+                int pa, pb, pc, pd;
+                case ' ':
+                    pa = find(baseId, parent);
+                    pb = find(baseId + 1, parent);
+                    pc = find(baseId + 2, parent);
+                    pd = find(baseId + 3, parent);
+                    merge(pa, pb, parent);
+                    merge(pa, pc, parent);
+                    merge(pa, pd, parent);
+                break;
+                case '/':
+                    pa = find(baseId + 3, parent);
+                    pb = find(baseId + 2, parent);
+                    merge(pa, pb, parent);
+                    pc = find(baseId + 1, parent);
+                    pd = find(baseId, parent);
+                    merge(pc, pd, parent);   
+                break;
+                case '\\':
+                    pa = find(baseId + 3, parent);
+                    pb = find(baseId, parent);
+                    merge(pa, pb, parent);
+                    pc = find(baseId + 2, parent);
+                    pd = find(baseId + 1, parent);
+                    merge(pc, pd, parent); 
+                break;
+                default:
+                break;
+            }
+            if (j > 0) {
+                int leftBaseId = (i * gridSize + j - 1) * 4;
+                int pa = find(leftBaseId + 3, parent);
+                int pb = find(baseId + 1, parent);
+                if (pa != pb) {
+                    merge(pa, pb, parent);
+                }
+            }
+            if (i > 0) {
+                int upBaseId = ((i - 1) * gridSize + j) * 4;
+                int pa = find(upBaseId + 2, parent);
+                int pb = find(baseId, parent);
+                if (pa != pb) {
+                    merge(pa, pb, parent);
+                }
+            }
+        }
+    }
+}
+
+int regionsBySlashes(char ** grid, int gridSize)
+{
+    int totalNodeNum = 4 * gridSize * gridSize;
+    int parent[totalNodeNum];
+
+    for (int i = 0; i < totalNodeNum; ++i) {
+        parent[i] = i;
+    }
+
+    mergeRegions(grid, gridSize, parent);
+
+    int res = 0;
+    for (int i = 0; i < totalNodeNum; ++i) {
+        if (i == find(i, parent)) {
+            res++;
+        }
+    }
+    
+    return res;
+}
+
+
 int printArray(int *array, int arraySize)
 {
     printf("res =");
@@ -177,6 +260,10 @@ int main(int argc, const char *argv[])
     int *ret1 = addToArrayForm(A, 4, 34, &returnSize1);
     printArray(ret1, returnSize1);
     /*******************************************************************************/
+
+    char *S = "\\/";
+    printf("%s %d %c\n", S, (int)strlen(S), S[1]);
+
     return 0;
 }
 
