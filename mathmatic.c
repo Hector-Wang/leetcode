db3a54cedbe1b8005ff9e7db399b1aa94f1f3863
Wang Haofeng <whf_zju@163.com>
2021-04-11 22:52:39 +0800

    finish 153. 寻找旋转排序数组中的最小值

--- mathmatic.c
+++ mathmatic.c
@@ -105,3 +105,18 @@ int findMin(int* nums, int numsSize)
     }
     return ret;
 }
+
+/*
+ * 153. 寻找旋转排序数组中的最小值
+ * https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array/
+ */
+int findMinI(int* nums, int numsSize)
+{
+    int ret = nums[0];
+    for (int i = 1; i < numsSize; ++i) {
+        if (nums[i] < nums[i - 1]) {
+            return nums[i];
+        }
+    }
+    return ret;
+}
