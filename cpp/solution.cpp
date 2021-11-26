254a2a5a606dff83bf70f9e628f323e14111096d
Wang Haofeng <whf_zju@163.com>
2021-11-26 23:27:17 +0800

    leetcode 700

--- cpp/solution.cpp
+++ cpp/solution.cpp
@@ -3,10 +3,114 @@
 #include <queue>
 #include <climits>
 #include <cmath>
+#include <cctype>
+#include <cstring>
 using namespace std;
 
 class Solution {
 public:
+
+    struct TreeNode {
+        int val;
+        TreeNode *left;
+        TreeNode *right;
+        TreeNode() : val(0), left(nullptr), right(nullptr) {}
+        TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    };
+
+/*
+ * 700. 二叉搜索树中的搜索
+ * https://leetcode-cn.com/problems/search-in-a-binary-search-tree/
+ */
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if (!root) {
+            return NULL;
+        }
+        if (root->val == val) {
+            return root;
+        }
+
+        if (root->val > val) {
+            return this->searchBST(root->left, val);
+        }
+
+        return this->searchBST(root->right, val);
+    }
+
+
+/*
+ * 有效的数独
+ * https://leetcode-cn.com/problems/valid-sudoku/
+ * 验证每一横行、竖列、九宫格是否满足数独要求即可
+ */ 
+    bool isValidSudoku(vector<vector<char>>& board) {
+        int assist[10];
+        for (int i = 0; i < board.size(); ++i) {
+            memset(assist, 0, sizeof(assist));
+            for (int j = 0; j < board[0].size(); ++j) {
+                if (isdigit(board[i][j])) {
+                    assist[board[i][j] - '0']++;
+                    if (assist[board[i][j] - '0'] > 1) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        for (int j = 0; j < board[0].size(); ++j) {
+            memset(assist, 0, sizeof(assist));
+            for (int i = 0; i < board.size(); ++i) {
+                if (isdigit(board[i][j])) {
+                    assist[board[i][j] - '0']++;
+                    if (assist[board[i][j] - '0'] > 1) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                memset(assist, 0, sizeof(assist));
+                for (int k = 0; k < 3; ++k) {
+                    for (int m = 0; m < 3; ++m) {
+                        if (isdigit(board[i * 3 + k][j * 3 + m])) {
+                            assist[board[i * 3 + k][j * 3 + m] - '0']++;
+                            if (assist[board[i * 3 + k][j * 3 + m] - '0'] > 1) {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+/*
+ * 1588. 所有奇数长度子数组的和
+ * https://leetcode-cn.com/problems/sum-of-all-odd-length-subarrays/
+ * 前缀和
+ */
+    int sumOddLengthSubarrays(vector<int>& arr) {
+        int sum = 0;
+        int size = arr.size();
+        vector<int> preSums(size + 1, 0);
+        for (int i = 0; i < size; ++i) {
+            preSums[i + 1] = preSums[i] + arr[i];
+        }
+
+        /* 对于每个元素，累加由该元素起始的 长度为1,3,5...的数组的和 */
+        for (int start = 0; start < size; ++start) {
+            for (int len = 1; start + len <= size; len += 2) {
+                sum += preSums[start + len] - preSums[start];
+            }
+        }
+
+        return sum;
+    }
+
 /*
  * 789. 逃脱阻碍者
  * https://leetcode-cn.com/problems/escape-the-ghosts/
